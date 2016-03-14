909d276de7749d6c621796914ab5394c60d77d1b
Tarah Nixon <erraticcreation@gmail.com>
2016-03-13 23:27:11 -0400

    updated gemfile to include guard gem and updated application configuration

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module SampleApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Allow web console to be used in test environment.
+    # config.web_console.development_only = false
   end
 end
