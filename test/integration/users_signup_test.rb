0140d72fab165d5837765de809b8ddc7ab03dc17
Tyler Nixon <ErraticCreation@gmail.com>
2016-03-21 21:45:38 -0400

    Updated Login behavior, and associated tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                         password_confirmation: "password"}
     end
     assert_template 'users/show'
+    assert is_logged_in?
     assert_select 'div#<error_explanation>'
     assert_select 'div.<alert-danger>'
     assert_not flash.alert
